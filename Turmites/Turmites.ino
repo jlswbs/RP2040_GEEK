d3af4056378b85894add92face216cc31c24b41d
jlswbs <jlswbs@users.noreply.github.com>
2024-06-11 08:46:27 +0200

    Add files via upload

---
+++ Turmites/Turmites.ino
@@ -0,0 +1,238 @@
+// Turmites cellular automata //
+
+#include "hardware/structs/rosc.h"
+#include "st7789_lcd.pio.h"
+
+#define PIN_DIN   11
+#define PIN_CLK   10
+#define PIN_CS    9
+#define PIN_DC    8
+#define PIN_RESET 12
+#define PIN_BL    25
+
+PIO pio = pio0;
+uint sm = 0;
+uint offset = pio_add_program(pio, &st7789_lcd_program);
+
+#define BLACK   0x0000
+#define BLUE    0x001F
+#define RED     0xF800
+#define GREEN   0x07E0
+#define CYAN    0x07FF
+#define MAGENTA 0xF81F
+#define YELLOW  0xFFE0
+#define WHITE   0xFFFF
+
+#define WIDTH   240
+#define HEIGHT  135
+#define SCR     (WIDTH*HEIGHT)
+
+#define ITER    125
+#define GENNUM  750
+
+  uint16_t col[SCR];
+  int posx, posy;
+  int oldposx, oldposy;
+  int state;
+  int dir, gen;
+  int world[WIDTH][HEIGHT];
+  int last_filled;
+  int current_col;
+  int next_col[4][4];
+  int next_state[4][4];
+  int directions[4][4];
+
+#define SERIAL_CLK_DIV 1.f
+
+static const uint8_t st7789_init_seq[] = {
+  
+  1, 20, 0x01,                        // Software reset
+  1, 10, 0x11,                        // Exit sleep mode
+  2, 2, 0x3a, 0x55,                   // Set colour mode to 16 bit
+  2, 0, 0x36, 0x70,                   // Set MADCTL: row then column, refresh is bottom to top ????
+  5, 0, 0x2a, 0x00, 0x28, 0x01, 0x17, // CASET: column addresses from 0 to 240 (f0)
+  5, 0, 0x2b, 0x00, 0x35, 0x00, 0xbb, // RASET: row addresses from 0 to 240 (f0)
+  1, 2, 0x21,                         // Inversion on, then 10 ms delay (supposedly a hack?)
+  1, 2, 0x13,                         // Normal display on, then 10 ms delay
+  1, 2, 0x29,                         // Main screen turn on, then wait 500 ms
+  0                                   // Terminate list
+
+};
+
+static inline void lcd_set_dc_cs(bool dc, bool cs) {
+
+  sleep_us(1);
+  gpio_put_masked((1u << PIN_DC) | (1u << PIN_CS), !!dc << PIN_DC | !!cs << PIN_CS);
+  sleep_us(1);
+
+}
+
+static inline void lcd_write_cmd(PIO pio, uint sm, const uint8_t *cmd, size_t count) {
+
+  st7789_lcd_wait_idle(pio, sm);
+  lcd_set_dc_cs(0, 0);
+  st7789_lcd_put(pio, sm, *cmd++);
+  if (count >= 2) {
+  st7789_lcd_wait_idle(pio, sm);
+  lcd_set_dc_cs(1, 0);
+  for (size_t i = 0; i < count - 1; ++i) st7789_lcd_put(pio, sm, *cmd++);
+  }
+  st7789_lcd_wait_idle(pio, sm);
+  lcd_set_dc_cs(1, 1);
+
+}
+
+static inline void lcd_init(PIO pio, uint sm, const uint8_t *init_seq) {
+
+  const uint8_t *cmd = init_seq;
+  while (*cmd) {
+  lcd_write_cmd(pio, sm, cmd + 2, *cmd);
+  sleep_ms(*(cmd + 1) * 5);
+  cmd += *cmd + 2;
+  }
+}
+
+static inline void st7789_start_pixels(PIO pio, uint sm) {
+
+  uint8_t cmd = 0x2c; // RAMWR
+  lcd_write_cmd(pio, sm, &cmd, 1);
+  lcd_set_dc_cs(1, 0);
+
+}
+
+static inline void seed_random_from_rosc(){
+  
+  uint32_t random = 0;
+  uint32_t random_bit;
+  volatile uint32_t *rnd_reg = (uint32_t *)(ROSC_BASE + ROSC_RANDOMBIT_OFFSET);
+
+  for (int k = 0; k < 32; k++) {
+    while (1) {
+      random_bit = (*rnd_reg) & 1;
+      if (random_bit != ((*rnd_reg) & 1)) break;
+    }
+
+    random = (random << 1) | random_bit;
+  }
+
+  srand(random);
+}
+
+void rndrule(){
+
+  memset(col,0,2*SCR);
+
+  state = rand()%4;
+  dir = 0;
+  posx = WIDTH/2;
+  posy = HEIGHT/2;
+  
+  for(int j=0; j<4; j++){   
+    for(int i=0; i<4; i++){         
+      next_col[i][j] = rand()%4;
+      next_state[i][j] = rand()%4;
+      directions[i][j] = rand()%8;
+    }   
+  }
+
+  for (int j=0; j < HEIGHT; j++){ for (int i = 0; i < WIDTH; i++) world[i][j]=0; } 
+
+  world[posx][posy] = rand()%4;
+
+}
+
+void move_turmite(){
+  
+  int cols = world[posx][posy];
+  
+  oldposx = posx;
+  oldposy = posy;
+  current_col = next_col[cols][state];
+  world[posx][posy] = next_col[cols][state];
+  state = next_state[cols][state];    
+
+  dir = (dir + directions[cols][state]) % 8;
+
+  switch(dir){
+    case 0: posy--; break;
+    case 1: posy--; posx++; break;
+    case 2: posx++; break;
+    case 3: posx++; posy++; break;
+    case 4: posy++; break;
+    case 5: posy++; posx--; break;
+    case 6: posx--; break;
+    case 7: posx--; posy--; break;
+  }
+
+  if(posy < 0) posy = HEIGHT-1;
+  if(posy >= HEIGHT) posy = 0;
+  if(posx < 0) posx = WIDTH-1;
+  if(posx >= WIDTH) posx=0;
+  
+}
+
+void setup() {
+
+  st7789_lcd_program_init(pio, sm, offset, PIN_DIN, PIN_CLK, SERIAL_CLK_DIV);
+
+  gpio_init(PIN_CS);
+  gpio_init(PIN_DC);
+  gpio_init(PIN_RESET);
+  gpio_init(PIN_BL);
+  gpio_set_dir(PIN_CS, GPIO_OUT);
+  gpio_set_dir(PIN_DC, GPIO_OUT);
+  gpio_set_dir(PIN_RESET, GPIO_OUT);
+  gpio_set_dir(PIN_BL, GPIO_OUT);
+
+  gpio_put(PIN_CS, 1);
+  gpio_put(PIN_RESET, 1);
+  lcd_init(pio, sm, st7789_init_seq);
+  gpio_put(PIN_BL, 1);
+
+  seed_random_from_rosc();
+
+  rndrule();
+  
+}
+
+
+void loop() {
+
+  st7789_start_pixels(pio, sm);
+
+  for (int i = 0; i < ITER; i++) {
+
+    move_turmite();
+
+    switch(current_col){
+      case 0: col[oldposx + oldposy * WIDTH] = RED; break;
+      case 1: col[oldposx + oldposy * WIDTH] = GREEN; break;
+      case 2: col[oldposx + oldposy * WIDTH] = BLUE; break;
+      case 3: col[oldposx + oldposy * WIDTH] = WHITE; break;
+    }
+    
+  }
+
+  for (int y = 0; y < HEIGHT; y++) {
+
+    for (int x = 0; x < WIDTH; x++) {
+
+      uint16_t coll = col[x + y * WIDTH];
+
+      st7789_lcd_put(pio, sm, coll >> 8);
+      st7789_lcd_put(pio, sm, coll & 0xff);
+
+    }
+
+  }
+
+  if (gen == GENNUM) {
+    
+    rndrule();
+    gen = 0;
+
+  }
+
+  gen++;
+ 
+}
